aee69f5f99773a5520104eeb14ed54f2275ff672
sultanhabibllah <h.ayodele@alustudent.com>
2026-09-17 15:41:17 -0700

    Add matrix definiteness function

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module for calculating matrix definiteness."""
+import numpy as np
+
+
+def definiteness(matrix):
+    """Calculate the definiteness of a matrix."""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    if matrix.size == 0:
+        return None
+    if not np.allclose(matrix, matrix.T):
+        return None
+
+    eigenvalues = np.linalg.eigvalsh(matrix)
+
+    if np.all(eigenvalues > 0):
+        return "Positive definite"
+    if np.all(eigenvalues >= 0):
+        return "Positive semi-definite"
+    if np.all(eigenvalues < 0):
+        return "Negative definite"
+    if np.all(eigenvalues <= 0):
+        return "Negative semi-definite"
+    return "Indefinite"
