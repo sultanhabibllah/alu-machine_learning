91b514421c11b5b94f10ac57a87d4905a9e818d2
sultanhabibllah <h.ayodele@alustudent.com>
2026-09-17 15:12:58 -0700

    Add NumPy element-wise operations

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for NumPy element-wise operations."""
+
+
+def np_elementwise(mat1, mat2):
+    """Perform element-wise operations."""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
